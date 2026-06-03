eb8cb793c702624b5edc8bedef2c07db1c58719f
zxb92699 <118104968+zxb92699@users.noreply.github.com>
2026-06-03 20:26:28 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,34 @@
+// 开场页
+
 const enterBtn = document.getElementById("enterBtn");
 
-if(enterBtn){
+if (enterBtn) {
 
-    enterBtn.addEventListener("click",()=>{
+    enterBtn.addEventListener("click", () => {
+
+        // 音乐播放
 
         const bgm = document.getElementById("bgm");
 
-        if(bgm){
-            bgm.play();
+        if (bgm) {
+
+            bgm.play().catch(() => {
+                console.log("浏览器阻止自动播放");
+            });
+
         }
+
+        // 开场页淡出
 
         const intro = document.getElementById("intro");
 
-        intro.style.opacity="0";
+        intro.style.opacity = "0";
 
-        setTimeout(()=>{
-            intro.style.display="none";
-        },1000);
+        setTimeout(() => {
+
+            intro.style.display = "none";
+
+        }, 1000);
 
     });
 
