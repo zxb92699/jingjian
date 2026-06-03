195e9ab0e5e7f4d7f00d422d0fc5e1e59c61990c
zxb92699 <118104968+zxb92699@users.noreply.github.com>
2026-06-03 15:22:44 +0800

    Initial website upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+const items=document.querySelectorAll('.fade');
+const io=new IntersectionObserver(es=>{
+es.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')})
+});
+items.forEach(i=>io.observe(i));
+const enterBtn = document.getElementById("enterBtn");
+
+if (enterBtn) {
+
+    enterBtn.addEventListener("click", () => {
+
+        document.getElementById("bgm").play();
+
+        document.getElementById("intro").style.opacity = "0";
+
+        setTimeout(() => {
+            document.getElementById("intro").style.display = "none";
+        }, 1000);
+
+    });
+
+}
